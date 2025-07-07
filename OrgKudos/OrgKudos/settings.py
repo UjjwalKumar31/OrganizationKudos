9a69e3168f32603df985f72a3fdbd8efa009086b
UjjwalKumar31 <ukg.ujjwalgupta@gmail.com>
2025-07-07 12:12:25 +0530

    Configure custom user model and REST framework

--- OrgKudos/OrgKudos/settings.py
+++ OrgKudos/OrgKudos/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'Kudos',
 ]
 
 MIDDLEWARE = [
@@ -120,3 +122,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'Kudos.User'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+    ]
+}
